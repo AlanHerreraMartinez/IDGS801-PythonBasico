b1fdaa6086ca9265f91a73e43b0cf433ec45915d
Alan Herrera Martinez 801 IDGS 20001575 <alanherreramartinez441@gmail.com>
2023-01-24 17:22:05 -0600

    Trabajo en clase 23/01/23

---
+++ tema13_my_modulo.py
@@ -0,0 +1,14 @@
+
+def tem():
+        print("!Hola desde my_modilo.py!")
+
+def tem2():
+        print("!adios desde my_modilo.py!")
+
+def main():
+        print("!Hola desde main!")
+        tem()
+        tem2()
+
+if __name__ == '__main__':
+        main()
